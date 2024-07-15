425b46259323809ff3a461fb7594b8a82a74e2fc
Miantsa2 <ranaivosonmiantsa@gmail.com>
2024-07-15 18:49:34 +0300

    rendez vous et template

---
+++ pi_view/rendezvous.php
@@ -0,0 +1,54 @@
+<div id="about" class="section layout_padding">
+         <div class="container">
+            
+            <div class="row">
+
+                 <div class="col-lg-5 margin_top_10">
+               <div class="full margin_top_10"">
+                  <h3 class="main_heading _left_side margin_top_10"> Prendre Rendez-vous</h3>
+                
+
+                  <form action="" method="">
+                    <div class="col-md-12">
+                    <div class="full field">
+                        <input type="datetime-local" placeholder="Date" name="datedebut" />
+                    </div>
+                    </div>
+                    <div class="col-md-12">
+                                <div class="full field">
+                                    <select name=""  style="width: 100%;border: none;background: #e8e8e8;min-height: 52px;padding: 0 20px;font-size: 14px;font-weight: 400;margin: 10px 0;">
+                                        <option value="" disabled selected >Type de Service</option>
+                                        <option value="">v8</option>
+                                        <option value="">v8</option>
+
+                                    </select>
+                            
+                                </div>
+                              </div>
+                             
+                    
+                    <div class="col-md-12">
+                
+                    <div class="full center" >
+                        <button class="submit_bt" type= "submit">Valider</button>
+                    </div>
+                  
+                    
+                    </div>
+        
+    
+                </form>
+               </div>
+              
+            </div>
+
+            <div class="col-lg-7">
+               <div class="full margin_top_50_rs">
+                  <img class="img-responsive" src="<?php echo base_url("assets/images/about_us.png")?>" alt="#" />
+               </div>
+            </div>
+
+               </div>
+
+         </div>
+      </div>
