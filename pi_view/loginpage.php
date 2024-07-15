f14ede5154a75e10d22a0d2f1cbf1b9c4e223a45
Miantsa2 <ranaivosonmiantsa@gmail.com>
2024-07-15 16:00:42 +0300

    login

---
+++ pi_view/loginpage.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <!-- basic -->
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <!-- mobile metas -->
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+      <!-- site metas -->
+      <title>avalon</title>
+      <meta name="keywords" content="">
+      <meta name="description" content="">
+      <meta name="author" content="">
+      <!-- bootstrap css -->
+      <link rel="stylesheet" href="css/bootstrap.min.css">
+      <!-- style css -->
+      <link rel="stylesheet" href="css/style.css">
+      <!-- Responsive-->
+      <link rel="stylesheet" href="css/responsive.css">
+      <!-- fevicon -->
+      <link rel="icon" href="" type="image/gif" />
+      <!-- Scrollbar Custom CSS -->
+      <link rel="stylesheet" href="css/jquery.mCustomScrollbar.min.css">
+      <!-- awesome fontfamily -->
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+      <!-- Tweaks for older IEs-->
+      <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+   </head>
+   <!-- body -->
+   <body class="main-layout">
+    
+   
+   <div class="wrapper">
+
+
+
+      <div id="content">
+
+
+      <!-- section -->
+      <section id="home" class="top_section">
+         <div class="row">
+            <div class="col-lg-12">
+               <!-- header -->
+      <header>
+         <!-- header inner -->
+         <div class="container">
+            <div class="row">
+               <div class="col-lg-3 logo_section">
+                  <div class="full">
+                     <div class="center-desk">
+                        <div class="logo"> <a href="index.html"><img style="width:100px;" src="images/logo2.png" alt="#"></a> </div>
+                     </div>
+                  </div>
+               </div>
+               <div class="col-lg-9">
+                  <div class="right_header_info">
+                     <ul>
+                 
+                        <div class="button_section">
+                        <a href="#">Admin</a>
+                        <!-- <a href="about.html">About Us</a> -->
+                     </div>
+                      
+                        
+                     </ul>
+                  </div>
+               </div>
+            </div>
+         </div>
+         <!-- end header inner -->
+      </header>
+      <section>
+         <div class="container-fluid">
+            <div class="row">
+               <div class="col-md-5">
+                  <div class="full slider_cont_section">
+                     <h4>Bienvenue</h4>
+                     <p>Veuillez vous connecter en remplissant les champs</p>
+           
+                            <form action="" method="">
+                               <div class="col-md-12">
+                                <div class="full field">
+                                  <input type="text" placeholder="Numero de la voiture" name="type" />
+                                </div>
+                              </div>
+                              <div class="col-md-12">
+                                <div class="full field">
+                                    <select name=""  style="width: 100%;border: none;background: #e8e8e8;min-height: 52px;padding: 0 20px;font-size: 14px;font-weight: 400;margin: 10px 0;">
+                                        <option value="" disabled selected >Type de la voiture</option>
+                                        <option value="">v8</option>
+                                        <option value="">v8</option>
+
+                                    </select>
+                            
+                                </div>
+                              </div>
+                             
+                               
+                             
+                              <div class="col-md-12">
+                                <div class="full center">
+                                  <button class="submit_bt">Se Connecter</button>
+                                </div>
+                              
+                              </div>
+                  
+                
+                            </form>
+                        </div>
+                     <!-- </div> -->
+                  <!-- </div> -->
+               </div>
+               <div class="col-md-7">
+                 <div id="slider_main" class="carousel slide" data-ride="carousel">
+                     <!-- The slideshow -->
+                     <div class="carousel-inner">
+                        <div class="carousel-item active">
+                           <img src="images/slider_1.png" alt="#" />
+                        </div>
+                       
+                     </div>
+                 
+                  </div>
+               </div>
+            </div>
+         </div>
+      </section>
+      <!-- end header -->
+            </div>
+         </div>
+      </section>
+      <!-- end section -->
+      
+      
+</div>
+</div>
+
+      
+
+     
+   </body>
+</html>
