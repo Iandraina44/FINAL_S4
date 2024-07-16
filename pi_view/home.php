951612691a5a68cc7d7d0027eb5304a9bfe2c626
Miantsa2 <ranaivosonmiantsa@gmail.com>
2024-07-16 10:40:10 +0300

    home back et front + mise a jour template front Office

---
+++ pi_view/home.php
@@ -0,0 +1,27 @@
+<div id="about" class="section layout_padding">
+         <div class="container">
+            
+            <div class="row">
+
+            <div class="col-md-5">
+                  <div class="full slider_cont_section">
+                     <h4>Bienvenue</h4>
+                     <h3 style="color:black">GULF</h3>
+                     <p style="color:black">Votre garage avec multiple services</p>
+                    
+                  </div>
+               </div>
+
+               
+
+            <div class="col-md-7">
+               <div class="full margin_top_50_rs">
+                  <img class="img-responsive"  src="<?php echo base_url("assets/images/garage.png")?>" alt="#" />
+               </div>
+            </div>
+
+               </div>
+
+         </div>
+      </div>
+    
---
+++ pi_view/homeAdim.php
@@ -0,0 +1,26 @@
+<div id="about" class="section layout_padding">
+         <div class="container">
+            
+            <div class="row">
+
+            <div class="col-md-5">
+                  <div class="full slider_cont_section">
+                     <h4>Bienvenue</h4>
+                     <h3 style="color:black">GULF</h3>
+                     <p style="color:black">Votre garage avec multiple services, vous etes en BackOffice</p>
+                    
+                  </div>
+               </div>
+
+               
+
+            <div class="col-md-7">
+               <div class="full margin_top_50_rs">
+                  <img class="img-responsive"  src="<?php echo base_url("assets/images/home2.jpg")?>" alt="#" />
+               </div>
+            </div>
+
+               </div>
+
+         </div>
+      </div>
