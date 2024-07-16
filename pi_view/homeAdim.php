f5f4eb45733d703a11be7b38c243b25a7632a826
Miantsa2 <ranaivosonmiantsa@gmail.com>
2024-07-16 10:53:13 +0300

    bouton supprimer donnes dans la base

--- pi_view/homeAdim.php
+++ pi_view/homeAdim.php
@@ -8,6 +8,10 @@
                      <h4>Bienvenue</h4>
                      <h3 style="color:black">GULF</h3>
                      <p style="color:black">Votre garage avec multiple services, vous etes en BackOffice</p>
+                     <div class="button_section">
+                        <a href="#" style="width:400px">Supprimer les donnees de la base</a>
+                      
+                     </div>
                     
                   </div>
                </div>
@@ -18,6 +22,7 @@
                <div class="full margin_top_50_rs">
                   <img class="img-responsive"  src="<?php echo base_url("assets/images/home2.jpg")?>" alt="#" />
                </div>
+               
             </div>
 
                </div>
