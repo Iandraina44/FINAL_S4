f441d86929a00bef9ffa7ff8997fc7267be08b21
Miantsa2 <ranaivosonmiantsa@gmail.com>
2024-07-16 11:29:43 +0300

    assets + importation

---
+++ pi_view/importation.php
@@ -0,0 +1,52 @@
+<div id="about" class="section layout_padding">
+         <div class="container">
+            
+            <div class="row">
+
+                 <div class="col-lg-5 margin_top_10">
+               <div class="full margin_top_10"">
+                  <h3 class="main_heading _left_side margin_top_10"> Importation Donnees</h3>
+                
+
+                  <form action="" method="">
+                    <div class="col-md-12">
+                    <div class="full field">
+                    <label for="fileInput" class="file-label" id="fileLabel" >Services</label>
+
+                        <input type="file" style="padding-top: 10px;" placeholder="Services" name="services" />
+                    </div>
+                    </div>
+                    <div class="col-md-12">
+                    <div class="full field">
+                    <label for="fileInput" class="file-label" id="fileLabel">Travaux</label>
+                     <input type="file" style="padding-top: 10px;" placeholder="Travaux" name="travaux" />
+                    </div>
+                    </div>
+                  
+                    
+                    <div class="col-md-12">
+                
+                    <div class="full center" >
+                        <button class="submit_bt" type= "submit">Importer</button>
+                    </div>
+                  
+                    
+                    </div>
+        
+    
+                </form>
+               </div>
+              
+            </div>
+
+            <div class="col-lg-7">
+               <div class="full margin_top_50_rs">
+                  <img class="img-responsive" src="<?php echo base_url("assets/images/gaar.jpg")?>" alt="#" />
+               </div>
+            </div>
+
+               </div>
+
+         </div>
+      </div>
+      
